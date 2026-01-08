7d1a6f66067d9e8fd2468601a3fcd4c0b8c257a8
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2026-01-09 01:33:22 +0530

    add: login functionality.

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,9 +1,22 @@
-import { getUserList } from "../utils/users.utils.js"
-
-export function login(req, res) {
-    //authenticate the user
-
-    console.log(getUserList())
-    const {email, password} = req.body
-    
+import { getUserList } from "../utils/users.utils.js";
+import bcrpyt from "bcrypt";
+export async function login(req, res) {
+  try {
+    const data = await getUserList();
+    const { email, password } = req.body;
+    const user = data.find((user) => user.email == email);
+    // console.log(user);
+    if (!user) {
+      return res.status(404).json({ msg: "User Not Found" });
+    } else {
+      if (await bcrpyt.compare(password, user.password)) {
+        return res.json({ msg: "Everything is Ok" });
+      } else {
+        return res.json({ msg: "Password is Wrong" });
+      }
+    }
+  } catch (e) {
+    res.sendStatus(500)
+    throw Error ({msg : e.message})
+  }
 }
