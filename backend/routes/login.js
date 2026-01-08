78fe36b8a1ad84b63ddd12ace1abc8234e02ee48
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2026-01-09 01:53:15 +0530

    add: login functionality.

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,22 +1,31 @@
 import { getUserList } from "../utils/users.utils.js";
+import { generateAccessToken } from "../services/token.service.js";
 import bcrpyt from "bcrypt";
+
 export async function login(req, res) {
   try {
     const data = await getUserList();
     const { email, password } = req.body;
+    //if email or password doesn't exist in the body.
+    if (!email || !password) {
+      return res.status(400).json({ msg: "Missing credentials" });
+    }
     const user = data.find((user) => user.email == email);
     // console.log(user);
+    const { id, role, name } = user;
+    //if user doesn't exist.
     if (!user) {
       return res.status(404).json({ msg: "User Not Found" });
-    } else {
-      if (await bcrpyt.compare(password, user.password)) {
-        return res.json({ msg: "Everything is Ok" });
-      } else {
-        return res.json({ msg: "Password is Wrong" });
-      }
     }
+    //password checking 
+    if (await bcrpyt.compare(password, user.password)) {
+      const token = generateAccessToken({ id, role, name, email });
+      return res.json({ token });
+    }
+
+    return res.json({ msg: "Password is Wrong" });
   } catch (e) {
-    res.sendStatus(500)
-    throw Error ({msg : e.message})
+    res.sendStatus(500);
+    throw Error({ msg: e.message });
   }
 }
