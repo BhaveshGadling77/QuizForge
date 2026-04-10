82babaa5e032287dc22b9a58a164298bee9631a8
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2026-04-10 23:04:02 +0530

    fix: frontend URL

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-  origin: "https://quiz-forge-1wqc.vercel.app/",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.use(express.urlencoded({ extended: true }))
